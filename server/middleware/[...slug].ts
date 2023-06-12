dc7fb2f848477ceb37479e4b290c9213cf2aae1a
devzota <111525617+devzota@users.noreply.github.com>
2023-06-12 11:57:45 +0700

    add coot.info
    add coot.info

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -12,6 +12,6 @@
 export default defineEventHandler((event) => {
     const headers = event.req.headers;
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        sendRedirect(event,"https://bigc43.bio"+event.req.url);
+        sendRedirect(event,"https://coot.info"+event.req.url);
     }
 })
